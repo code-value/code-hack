42565a184493baa87bb496da3deb07a340cb31b9
Parth Hetalkumar Mistry <imparthmistry@gmail.com>
2020-10-04 14:03:51 +0530

    added one-liner function

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -1,16 +1,19 @@
-def fibonacci(n): 
+def fibonacci(n):
     a = 0
     b = 1
-    if n == 0: 
-        return a 
-    elif n == 1: 
-        return b 
-    else: 
-        for i in range(2, n+1): 
-            c = a + b 
-            a = b 
-            b = c 
-        return b 
-  
-print(fibonacci(10)) 
-  
+    if n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+# added one-liner
+fib = lambda n: n if n<2 else fib(n-1)+fib(n-2)
+
+print(fibonacci(10))
+print(fib(10))
